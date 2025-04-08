177d17a017b13f1c3da99c91e5f57b2e23dd5dbc
AAGNESH07 <aagneshpandu@gmail.com>
2025-04-08 21:23:46 +0530

    typecasting

---
+++ javascript/typecasting.js
@@ -0,0 +1,66 @@
+//let runs = "hey"+ "You";
+//console.log(runs);
+
+//let runs1 =4*400;
+//console.log(runs1); //
+
+//let age=prompt("how old are you");
+//console.log(typeof age);
+//age=Number(age);
+//console.log("happy birthday, your age ",age)
+
+
+//let runs = prompt("Enter your runs:");
+//let num = Number(runs); 
+//let extras=10
+//totalruns= runs*extras
+//console.log("You entered:", num);
+//console.log(extras+runs);
+
+
+    //  let userInput = prompt("Enter a number:");
+      //let num1 = Number(userInput);
+      //console.log("You entered:", num1);
+      //console.log("Doubled value:", num1 * 2);
+
+
+
+    //implicit
+
+    //string to number
+    //let player=prompt("enter your number")
+    //let runs=player*2;
+    //console.log(runs)
+// String + Number
+    //let player1=prompt("enter your number")
+    //let runs1=player1+2;
+    //console.log(runs1)
+
+    //true/false
+    let udayruns=prompt("enter your runs");
+    if (udayruns>=50) {
+      console.log("you hits half century")
+    }
+    else{
+        console.log("not half century")
+    }
+
+// '=='
+    let input = prompt("Enter 10:");
+    if (input == 10) {  
+        console.log("You entered ten!");
+    } else {
+        console.log("Not ten");
+    }
+
+//Eplicit typecasting
+
+//input string to number   
+      let input1 = prompt("Enter a number:");
+      let num1 = Number(input1); 
+      console.log("Converted Number:", num1);
+
+//boolean
+let input2 = prompt("Enter something:");
+let boolValue = Boolean(input2); 
+console.log("Converted Boolean:", boolValue);       
