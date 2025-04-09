cb014a923fcb7016b9e124e3b836f1425189a886
AAGNESH07 <aagneshpandu@gmail.com>
2025-04-09 22:42:06 +0530

    conditional statements

---
+++ conditional/condition.js
@@ -0,0 +1,28 @@
+//if statement
+
+//let age = prompt("enter your age")//20
+
+//if(age >=18 ){
+    //console.log("your are adult");//your  are adult
+//}
+
+//is-else statement
+//let age1 = Number(prompt("Enter your age"));
+
+//if (age1 >= 18) {
+    //console.log("You are an adult");
+//} else {
+    //console.log("You are not an adult");
+//}
+
+
+//if-else-if statement
+let score = prompt("enter you score")
+
+if(score >= 100){
+    console.log("you hit century")
+}else if (score >= 50){
+    console.log("you hit half century")
+} else{
+    console.log("you scored less runs")
+}
